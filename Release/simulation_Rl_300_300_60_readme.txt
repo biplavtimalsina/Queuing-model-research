b037280336d1427ddf112a147b6df1f2ecbe6cf4
biplavtimalsina <btimalsina@nevada.unr.edu>
2018-05-21 16:03:44 -0700

    Add files via upload

---
+++ Release/simulation_Rl_300_300_60_readme.txt
@@ -0,0 +1,3 @@
+simulation_Rl_300_300_60
+
+repeated requests with IAR 60 for 300 time frames to find out when the value of PI converges
---
+++ Release/test_p.ps1
@@ -0,0 +1,6 @@
+
+
+rm Diff_dist_Exp_Er_Nor_10.txt
+./DNNSimulator.exe $iMachNo $totalthreadNum $iThreadSerNum $arrmean $maxReqNum >> Diff_dist_Exp_Er_Nor_10.txt
+
+
